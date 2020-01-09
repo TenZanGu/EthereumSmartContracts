ccced473adf32f464703f94fa92998375f43ec5a
Yuet Loo Wong <contact@yuetloo.com>
2020-01-09 16:51:05 -0500

    add fallback bug

---
+++ Fallback.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.15;
+
+contract Bank {
+    mapping(address=>uint) public balances;
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    function () external payable {}
+}
+
+contract User {
+    function deposit(address payable bank) public payable {
+        (bool success, ) = bank.call.value(msg.value)(abi.encodeWithSignature("deposit()"));
+        require(success);
+    }
+    
+    function badDeposit(address payable bank) public payable {
+        (bool success, ) = bank.call.value(msg.value)(abi.encodeWithSignature("Deposit()"));
+        require(success);
+    }
+}
